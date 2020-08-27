376f1d85bf2d39c96efe057e0242395b52b50cc5
ssr <sheetalsinghnew@gmail.com>
2020-08-27 14:32:55 +0530

    complete

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React,{useState} from 'react';
+const api={
+  key:"3c9b4f9ca2fcdbc910fc9cebbf7fe8b6",
+  base:"https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+
+  const [query,setQuery] = useState("");
+  const [weather,setWeather] = useState({});
+
+  const search = evt => {
+    if(evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then( result =>{
+        setWeather(result);
+        setQuery("");
+        console.log(weather);
+      }
+        );
+    }
+  }
+  const dateBuilder = (d) => {
+    let months =["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
+    let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+  return (
+    <div className="app">
+     <main>
+       <div className="search-box">
+          <input
+                type="text"
+                className="search-bar"
+                placeholder="Search..."
+                onChange = {e => { return setQuery(e.target.value); }}
+                value = {query}
+                onKeyPress = {search}/>
+       </div>
+       {(typeof weather.main != "undefined")? (
+         <div>
+           
+       <div className="location-box">
+         <div className="location">{weather.name}, {weather.sys.country}</div>
+         <div className="date">{dateBuilder(new Date())}</div>
+       </div>
+       <div className="weather-box">
+       <div className="temp">
+       {Math.round(weather.main.temp)}°C
+       </div>
+       <div className="weather">{weather.weather[0].main}</div>
+       </div>
+         </div>
+       ):("")}
+     </main>
+    </div>
+  );
+}
+
+export default App;
